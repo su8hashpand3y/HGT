a2eb58895ec500822ce5dd92ee6e4e0443663c80
Subhash <ce.subhash@gmail.com>
2018-05-08 10:56:25 +0530

    First Draft

--- HGT/ClientApp/app/components/home/home.component.ts
+++ HGT/ClientApp/app/components/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
     video1: string = "Media/SampleVideo_1280x720_1mb.mp4";
