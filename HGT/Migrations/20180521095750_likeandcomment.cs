cf77531c37cdc4471fffbd64d1061a20b315996a
Subhash <ce.subhash@gmail.com>
2018-05-21 15:36:22 +0530

    trying to connect to external server

---
+++ HGT/Migrations/20180521095750_likeandcomment.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HGT.Migrations
+{
+    public partial class likeandcomment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UniqueID",
+                table: "Videos");
+
+            migrationBuilder.AddColumn<long>(
+                name: "Comments",
+                table: "Videos",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comments",
+                table: "Videos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UniqueID",
+                table: "Videos",
+                nullable: true);
+        }
+    }
+}
